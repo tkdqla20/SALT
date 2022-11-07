7329d05b5601e9cef11797be11e6fb9f46102c01
tkdqla20 <95307257+tkdqla20@users.noreply.github.com>
2022-11-07 23:26:15 +0900

    TEST 1

---
+++ SALT_memo.js
@@ -0,0 +1,67 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.querySelector('#todo')
+    const addButton = document.querySelector('#add-button')
+    const todoList = document.querySelector('#todo-list')
+    const alert = document.querySelector('#kimchi')
+	
+	
+
+      // '+' 버튼 익명 화살표 함수 
+    const addTodo = () => { 
+        if (input.value !== '') {
+            const item = document.createElement('div')
+          // 체크박스
+            const checkbox = document.createElement('input')
+            checkbox.type='checkbox'
+         // text
+            const text = document.createElement('span');
+          // 제거하기 버튼
+            const deleteButton = document.createElement('button')
+            deleteButton.textContent="-"
+
+            text.textContent = input.value
+            input.value=''
+        
+			item.appendChild(deleteButton)
+            item.appendChild(checkbox)
+            item.appendChild(text)
+            todoList.appendChild(item)
+			
+			deleteButton.id = 'deleteButton';
+
+    // 체크박스 이벤트 리스너
+            checkbox.addEventListener('change', (event) =>{ 
+                if (event.currentTarget.checked)
+                {
+                    text.style.textDecoration='line-through'
+                }
+                else {
+                    text.style.textDecoration='none'
+                }
+            })
+
+          // 제거하기 버튼 클릭 이벤트 리스너
+            deleteButton.addEventListener('click', (event) => {
+                todoList.removeChild(event.currentTarget.parentNode)
+            })
+            input.value =''
+            alert.textContent = '입력 성공!'
+        }
+        else
+            alert.textContent = '할 일을 입력하세요!'
+    }
+
+    addButton.addEventListener('click', addTodo)
+
+      // 할 일 입력창에서 enter key가 눌렸을 때
+    input.addEventListener('keypress', (event) => {
+        const ENTER = 13
+        if (event.keyCode === ENTER)
+            addTodo();
+    })
+})
+
+
+localStorage.setItem('get#todo', 'get#todo-list');
+const input = localStorage.getItem('get#todo');
+const todoList = localStorage.getItem('get#todo-list');
