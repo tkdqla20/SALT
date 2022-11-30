808fa210ea3d760ea1ab8fb68dd93e66267fd18f
tkdqla20 <95307257+tkdqla20@users.noreply.github.com>
2022-11-30 23:26:42 +0900

    TEST 7

--- SALT_google.connect.js
+++ SALT_google.connect.js
@@ -2,40 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
 			var calendarEl = document.getElementById('calendar');
 			var calendar = new FullCalendar.Calendar(calendarEl, {
 				  locale: "ko",
-				  initialView: 'timeGridWeek',
+				  initialView: 'dayGridMonth',
+				
 				  headerToolbar: {
 					left: 'prev,next today',
 					center: 'title',
-					right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth addEventButton'
+					right: 'addEventButton'
 				  },
-					customButtons: {
+				customButtons: {
                     addEventButton: { // 추가한 버튼 설정
                         text : "일정 추가",  // 버튼 내용
                         click : function(){ // 버튼 클릭 시 이벤트 추가
-                            $("#calendarModal").modal("show"); // modal 나타내기
-
-                            $("#addCalendar").on("click",function(){  // modal의 추가 버튼 클릭 시
-                                var content = $("#calendar_content").val();
-                                var start_date = $("#calendar_start_date").val();
-                                var end_date = $("#calendar_end_date").val();
-                                
-                                //내용 입력 여부 확인
-                                if(content == null || content == ""){
-                                    alert("내용을 입력하세요.");
-                                }else if(start_date == "" || end_date ==""){
-                                    alert("날짜를 입력하세요.");
-                                }else if(new Date(end_date)- new Date(start_date) < 0){ // date 타입으로 변경 후 확인
-                                    alert("종료일이 시작일보다 먼저입니다.");
-                                }else{ // 정상적인 입력 시
-                                    var obj = {
-                                        "title" : content,
-                                        "start" : start_date,
-                                        "end" : end_date
-                                    }//전송할 객체 생성
-
-                                    console.log(obj); //서버로 해당 객체를 전달해서 DB 연동 가능
-                                }
-                            });
+							const modal = document.querySelector('.modal');
+                            modal.style.display = "block"
                         }
                     }
                 },
@@ -44,6 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
 				  googleCalendarId: 'f49778c019e9f8dcb541f44bcecca05357568e7cea513875edbbb49bf3d3d019@group.calendar.google.com',
 				  className: 'gcal-event' // an option!
 				  },
+				
 													 
 				  eventClick: function(info) {
 					  let start_year = info.event.start.getUTCFullYear();
@@ -86,15 +66,10 @@ document.addEventListener('DOMContentLoaded', function() {
 					  });
 					  info.jsEvent.stopPropagation();
 					  info.jsEvent.preventDefault();
+					  
+					  
 				  }
+				
 			});
 			calendar.render();
 		});
-
-
-
-function test() {
-	var googleCalendarApiKey = document.getElementById('id');
-	var googleCalendarId = document.getElementById('password');
-	localStorage.setItem(googleCalendarApiKey, googleCalendarId)
-}
